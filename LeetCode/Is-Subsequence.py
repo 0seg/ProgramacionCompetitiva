0c4e226ac848dd5372dcc9bff80ebe2b2d09ebf3
e <chessemi00@gmail.com>
2026-01-29 13:25:01 -0600

    Add Is-Subsequence solution implementation

---
+++ LeetCode/Is-Subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        pos = 0
+        if s == '': return True
+
+        for a in range(len(t)):
+            if t[a] == s[pos]:
+                pos+= 1
+                if pos  == len(s):
+                    return True
+        return False
