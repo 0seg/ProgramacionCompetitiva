a2de6a508f906fc009a2c893a0dcf9b9f4e966e8
e <chessemi00@gmail.com>
2026-03-20 19:07:21 -0600

    Add implementations for isPowerOfTwo, isPowerOfThree, and isPowerOfFour solutions

---
+++ LeetCode/231.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        while n > 1:
+            if n % 2 != 0:
+                return False
+            n //= 2
+        return True
