51ac5085d2e54a02d34d43efb4f21a8301f71cdd
0seg <chessemi00@gmail.com>
2026-09-02 21:33:08 -0600

    Add implementation for MyQueue class using two stacks

---
+++ LeetCode/232.cpp
@@ -0,0 +1,45 @@
+class MyQueue {
+    private:
+        stack<int> stack1;
+        stack<int> stack2;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        while(!stack1.empty()){
+            stack2.push(stack1.top());
+            stack1.pop();
+        }
+        stack1.push(x);
+
+        while(!stack2.empty()){
+            stack1.push(stack2.top());
+            stack2.pop();
+        }
+    }
+    
+    int pop() {
+        int front = stack1.top();
+        stack1.pop();
+        return front;
+    }
+    
+    int peek() {
+        return stack1.top();
+    }
+    
+    bool empty() {
+        return (stack1.empty() and stack2.empty());
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
